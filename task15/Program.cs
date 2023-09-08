9bc3ce9e0ccc5f155b5ae0aff7a7ebe4728bef79
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-08 16:41:20 +0300

    task15 finished

---
+++ task15/Program.cs
@@ -0,0 +1,26 @@
+﻿int GetNumber(string mess)
+{
+    Console.WriteLine($"Введите {mess}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int day = GetNumber("номер дня");
+if (day<8)
+{
+    switch (day)
+    {
+        case 6:
+            Console.WriteLine("Да");
+            break;
+        case 7:
+            Console.WriteLine("Да");
+         break;
+        default:
+        Console.WriteLine("Нет");
+        break;
+    }
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
