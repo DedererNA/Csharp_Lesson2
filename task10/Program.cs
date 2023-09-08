e0083edc71e706e492c15118c4d80a7c831997d8
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-08 16:31:28 +0300

    task10 finished

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetNumber(string mess)
+{
+    Console.WriteLine($"Введите {mess}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int num = GetNumber("число");
+if (num/100>0&&num/100<10)
+{
+    Console.WriteLine($"{num/10-(num/100)*10}");
+}
+else
+{
+    Console.WriteLine("Недопустимое число");
+}
